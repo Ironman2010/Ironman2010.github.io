f0b72d31607449256b98b9b8d1f3e66bdab317b5
Ironman2010 <61697223+Ironman2010@users.noreply.github.com>
2020-06-17 13:06:34 +0200

    Add files via upload

---
+++ review/screens/090a87ab-de0c-4124-91de-76318cb895ae.js
@@ -0,0 +1,100 @@
+var content='<div class="ui-page" deviceName="androidPhone" deviceType="mobile" deviceWidth="360" deviceHeight="640">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devMobile canvas firer commentable non-processed" alignment="left" name="Template 1" width="360" height="640">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1592385827389.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1592385827389-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1592385827389-ie8.css" /><![endif]-->\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+    <div id="s-090a87ab-de0c-4124-91de-76318cb895ae" class="screen growth-vertical devMobile canvas PORTRAIT firer commentable non-processed" alignment="left" name="Test" width="360" height="640">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/090a87ab-de0c-4124-91de-76318cb895ae-1592385827389.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/screens/090a87ab-de0c-4124-91de-76318cb895ae-1592385827389-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/screens/090a87ab-de0c-4124-91de-76318cb895ae-1592385827389-ie8.css" /><![endif]-->\
+      <div id="s-raised_Button" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="20" dataY="345" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_0">Huoltotaulu</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_1" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="129" dataY="345" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_1_0">Kohdetiedot</span><span id="rtr-s-raised_Button_1_1"><br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_2" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="239" dataY="345" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_2_0">Lue QR-Kood</span><span id="rtr-s-raised_Button_2_1">it</span><span id="rtr-s-raised_Button_2_2"><br /><br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_3" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="21" dataY="448" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_3_0">Tiedostot</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_4" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="130" dataY="448" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_4_0">Pohjakuvat</span><span id="rtr-s-raised_Button_4_1"><br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_5" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="240" dataY="448" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_5_0">Varanappi<br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Text_1" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="52px" datasizeheight="23px" dataX="153" dataY="10" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_1_0">MITRA</span></div></div></div></div>\
+\
+      <div id="s-Image_2" class="pie image firer ie-background commentable non-processed"   datasizewidth="119px" datasizeheight="113px" dataX="11" dataY="260"   alt="image">\
+          <img src="./images/ec8b236a-297d-4bb1-8639-f564729cf220.png" />\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
---
+++ review/screens/440816f9-8149-40b3-8de7-53950e349178.js
@@ -0,0 +1,104 @@
+var content='<div class="ui-page" deviceName="androidPhone" deviceType="mobile" deviceWidth="360" deviceHeight="640">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devMobile canvas firer commentable non-processed" alignment="left" name="Template 1" width="360" height="640">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1592385827389.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1592385827389-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1592385827389-ie8.css" /><![endif]-->\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+    <div id="s-440816f9-8149-40b3-8de7-53950e349178" class="screen growth-vertical devMobile canvas PORTRAIT firer commentable non-processed" alignment="left" name="Futuristic" width="360" height="640">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/440816f9-8149-40b3-8de7-53950e349178-1592385827389.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/screens/440816f9-8149-40b3-8de7-53950e349178-1592385827389-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/screens/440816f9-8149-40b3-8de7-53950e349178-1592385827389-ie8.css" /><![endif]-->\
+      <div id="s-Text_1" class="pie label singleline autofit firer ie-background commentable non-processed"   datasizewidth="61px" datasizeheight="26px" dataX="148" dataY="20" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Text_1_0">MITRA </span></div></div></div></div>\
+      <div id="s-raised_Button" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="20" dataY="389" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_0">Huoltotaulu</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_1" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="129" dataY="389" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_1_0">Kohdetiedot</span><span id="rtr-s-raised_Button_1_1"><br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_2" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="239" dataY="389" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_2_0">Lue QR-Kood</span><span id="rtr-s-raised_Button_2_1">it</span><span id="rtr-s-raised_Button_2_2"><br /><br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_3" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="21" dataY="492" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_3_0">Tiedostot</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_4" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="130" dataY="492" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_4_0">Pohjakuvat</span><span id="rtr-s-raised_Button_4_1"><br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-raised_Button_5" class="pie button multiline firer click commentable non-processed"   datasizewidth="100px" datasizeheight="80px" dataX="240" dataY="492" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-raised_Button_5_0">Varanappi<br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+\
+      <div id="s-Image_3" class="pie image firer ie-background commentable non-processed"   datasizewidth="119px" datasizeheight="113px" dataX="11" dataY="305"   alt="image">\
+          <img src="./images/ec8b236a-297d-4bb1-8639-f564729cf220.png" />\
+      </div>\
+\
+      <div id="s-Image_1" class="pie image lockV firer ie-background commentable non-processed"   datasizewidth="360px" datasizeheight="202px" dataX="0" dataY="78" aspectRatio="0.5611111"   alt="image">\
+          <img src="./images/56e6966c-0f94-45fe-8a6d-3343a3bcb384.gif" />\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
